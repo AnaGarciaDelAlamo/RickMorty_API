d03f926b52b7cc2bde2114d99f7b8c1998e2da08
anagd <andelamo@myuax.es>
2023-11-04 10:43:53 +0100

    Prueba4 con base de datos sin fallos pero sin actividad

--- app/src/main/java/com/example/rickmorty_api/SQL/CharacterDatabaseHelper.java
+++ app/src/main/java/com/example/rickmorty_api/SQL/CharacterDatabaseHelper.java
@@ -5,16 +5,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class CharacterDatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "FavoriteCharacters.db";
+    private static final String DATABASE_NAME = "CharacterFavorites.db";
     private static final int DATABASE_VERSION = 1;
 
-    public static final String TABLE_FAVORITE_CHARACTERS = "favorite_characters";
+    public static final String TABLE_FAVORITES = "favorites";
     public static final String COLUMN_ID = "id";
     public static final String COLUMN_NAME = "name";
-
-    private static final String DATABASE_CREATE = "create table " + TABLE_FAVORITE_CHARACTERS + " ("
-            + COLUMN_ID + " integer primary key autoincrement, "
-            + COLUMN_NAME + " text not null);";
 
     public CharacterDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,12 +18,16 @@ public class CharacterDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(DATABASE_CREATE);
+        String createTableSQL = "CREATE TABLE " + TABLE_FAVORITES + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY, " +
+                COLUMN_NAME + " TEXT" +
+                ");";
+        db.execSQL(createTableSQL);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_FAVORITE_CHARACTERS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_FAVORITES);
         onCreate(db);
     }
 }
--- app/src/main/java/com/example/rickmorty_api/character/CharacterDetailActivity.java
+++ app/src/main/java/com/example/rickmorty_api/character/CharacterDetailActivity.java
@@ -1,17 +1,28 @@
 package com.example.rickmorty_api.character;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.rickmorty_api.R;
+import com.example.rickmorty_api.SQL.CharacterDatabaseHelper;
 
 public class CharacterDetailActivity extends AppCompatActivity {
+
+    private Character character;
     private WebView detailWebView;
+
+    private SQLiteDatabase database;
+    private boolean isFavorite;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,13 +35,62 @@ public class CharacterDetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra("character")) {
-            Character character = (Character) intent.getSerializableExtra("character");
+            character = (Character) intent.getSerializableExtra("character"); // Asignar el objeto Character a la variable character
+
+            // Verificar si el personaje ya es un favorito
+            CharacterDatabaseHelper dbHelper = new CharacterDatabaseHelper(this);
+            database = dbHelper.getWritableDatabase();
+            isFavorite = checkIfFavorite(character.getName());
 
             String htmlData = generateCharacterDetailsHTML(character);
             detailWebView.loadDataWithBaseURL(null, htmlData, "text/html", "UTF-8", null);
         }
     }
 
+    public void addToFavorites(View view) {
+        if (isFavorite) {
+            removeFromFavorites(character.getName());
+            isFavorite = false;
+            Toast.makeText(this, "Removed from Favorites", Toast.LENGTH_SHORT).show();
+        } else {
+            addToFavorites(character.getName());
+            isFavorite = true;
+            Toast.makeText(this, "Added to Favorites", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void addToFavorites(String characterName) {
+        ContentValues values = new ContentValues();
+        values.put(CharacterDatabaseHelper.COLUMN_NAME, characterName);
+        database.insert(CharacterDatabaseHelper.TABLE_FAVORITES, null, values);
+    }
+
+    private void removeFromFavorites(String characterName) {
+        database.delete(CharacterDatabaseHelper.TABLE_FAVORITES,
+                CharacterDatabaseHelper.COLUMN_NAME + " = ?",
+                new String[]{characterName});
+    }
+
+    private boolean checkIfFavorite(String characterName) {
+        String[] columns = {CharacterDatabaseHelper.COLUMN_ID};
+        String selection = CharacterDatabaseHelper.COLUMN_NAME + " = ?";
+        String[] selectionArgs = {characterName};
+        Cursor cursor = database.query(
+                CharacterDatabaseHelper.TABLE_FAVORITES,
+                columns,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null
+        );
+        int count = cursor.getCount();
+        cursor.close();
+        return count > 0;
+    }
+
+
+    //Elementos webView
     private String generateCharacterDetailsHTML(Character character) {
         String htmlData = "<html><body>";
         htmlData += "<div style='text-align: center;'>";
